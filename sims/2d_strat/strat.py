ba04c822d3b6b90a3a4f5004de42298c72449650
Yubo Su <yubo@blend.com>
2018-03-06 17:05:33 -0500

    fix sponge, rm steady

--- sims/2d_strat/strat.py
+++ sims/2d_strat/strat.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     KZ = 2 * np.pi / H
     G = 10
     OMEGA = strat_helper.get_omega(G, H, KX, KZ)
-    T_F = ZMAX / (OMEGA/KZ) * 5
+    T_F = ZMAX / (OMEGA/KZ) * 10
     DT = T_F / 3e4
 
     params = {'XMAX': H,
@@ -41,9 +41,8 @@ if __name__ == '__main__':
               'NUM_SNAPSHOTS': 200}
     # modified sponge params
     params_sponge = dict(params)
-    params_sponge['DT'] = 10 * DT
-    params['N_X'] = 32
-    params['N_Z'] = 128
+    params_sponge['N_X'] = 32
+    params_sponge['N_Z'] = 128
 
     def dirichlet_bc(problem, *_):
         strat_helper.default_problem(problem)
@@ -62,7 +61,7 @@ if __name__ == '__main__':
         '''
         puts a -gamma(z) * q damping on all dynamical variables, where gamma(z)
         is the sigmoid: damping * exp(steep * (z - z_sigmoid)) / (1 + exp(...))
-        '''
+       '''
         zmax = params['ZMAX']
         damp_start = zmax * 0.7 # start damping zone
         z = domain.grid(1)
@@ -70,16 +69,16 @@ if __name__ == '__main__':
         # sponge field
         sponge = domain.new_field()
         sponge.meta['x']['constant'] = True
-        sponge['g'] = np.minimum(
+        sponge['g'] = np.maximum(
             1 - (z - zmax)**2 / (damp_start - zmax)**2,
             np.zeros(np.shape(z)))
 
         problem.parameters['sponge'] = sponge
         problem.add_equation("dx(ux) + dz(uz) = 0")
-        problem.add_equation("dt(rho) - rho0 * uz / H - sponge * rho= 0")
-        problem.add_equation("dt(ux) + dx(P) / rho0 - sponge * ux= 0")
+        problem.add_equation("dt(rho) - rho0 * uz / H + sponge * rho= 0")
+        problem.add_equation("dt(ux) + dx(P) / rho0 + sponge * ux= 0")
         problem.add_equation(
-            "dt(uz) + dz(P) / rho0 + rho * g / rho0 - sponge * uz= 0")
+            "dt(uz) + dz(P) / rho0 + rho * g / rho0 + sponge * uz= 0")
 
         problem.add_bc("left(P) = 0", condition="nx == 0")
         problem.add_bc("left(uz) = cos(KX * x - omega * t)")
@@ -97,24 +96,6 @@ if __name__ == '__main__':
         uz['g'] = np.zeros(gshape)
         rho['g'] = np.zeros(gshape)
 
-    def steady_ic(solver, domain):
-        ux = solver.state['ux']
-        uz = solver.state['uz']
-        P = solver.state['P']
-        rho = solver.state['rho']
-        x = domain.grid(0)
-        z = domain.grid(1)
-
-        rho0 = params['RHO0']
-
-        common_factor = np.exp(z / (2 * H)) * np.sin(KZ * (ZMAX - z)) / \
-            np.sin(KZ * ZMAX)
-        uz['g'] = np.cos(KX * x) * common_factor
-        ux['g'] = -KZ / KX * np.cos(KX * x + 1 / (2 * H * KZ)) * common_factor
-        rho['g'] = -rho0 / (H * OMEGA) * np.sin(KX * x) * common_factor
-        P['g'] = rho0 * OMEGA * KZ / KX**2 *np.cos(KX * x + 1 / (2 * H * KZ)) \
-            * common_factor
-
     def run(bc, ic, name, params_dict):
         strat_helper.run_strat_sim(bc, ic, name=name, **params_dict)
         return '%s completed' % name
@@ -123,7 +104,6 @@ if __name__ == '__main__':
         tasks = [
             (dirichlet_bc, zero_ic, 'd0', params), # strat_dirichlet.mp4
             (neumann_bc, zero_ic, 'n0', params), # strat_neumann.mp4
-            (dirichlet_bc, steady_ic, 'dss', params), # strat_dirichlet_ss.mp4
             (sponge, zero_ic, 'sponge', params_sponge), # strat_sponge.mp4
         ]
         res = []
