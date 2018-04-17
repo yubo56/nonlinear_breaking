aca173864fbf0546b561e35957e75c5fd729d6d1
Yubo Su <yubo@blend.com>
2018-04-17 00:54:52 -0400

    commit README

--- sims/2d_2_strat_nonlin/strat.py
+++ sims/2d_2_strat_nonlin/strat.py
@@ -72,22 +72,22 @@ if __name__ == '__main__':
         (strat_helper.sponge_nonlin,
          strat_helper.bg_ic,
          'sponge_nonlin3',
-         build_interp_params(8, 4, overrides={'A': 0.01})),
+         build_interp_params(8, 4, overrides={'A': 0.02})),
 
         (strat_helper.sponge_nonlin,
          strat_helper.bg_ic,
          'sponge_nonlin4',
-         build_interp_params(8, 4, overrides={'A': 0.03})),
-
-        (strat_helper.sponge_nonlin,
-         strat_helper.bg_ic,
-         'sponge_nonlin5',
          build_interp_params(8, 4, overrides={'A': 0.04})),
 
         (strat_helper.sponge_nonlin,
          strat_helper.bg_ic,
+         'sponge_nonlin5',
+         build_interp_params(8, 4, overrides={'A': 0.1})),
+
+        (strat_helper.sponge_nonlin,
+         strat_helper.bg_ic,
          'sponge_nonlin6',
-         build_interp_params(8, 4, overrides={'A': 0.05})),
+         build_interp_params(8, 4, overrides={'A': 0.3})),
 
         # (rad_bc, zero_ic, 'rad', build_interp_params(8, 4)),
     ]
