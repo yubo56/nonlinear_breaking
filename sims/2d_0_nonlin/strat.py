854dbec0a0be201e39af3b69213b444696d0dad4
Yubo Su <yubo@blend.com>
2018-07-23 15:52:21 -0400

    fix timing calculations

--- sims/2d_0_nonlin/strat.py
+++ sims/2d_0_nonlin/strat.py
@@ -8,10 +8,6 @@ from strat_helper import *
 H = 1
 XMAX = 4 * H
 ZMAX = 10 * H
-KX = 2 * np.pi / XMAX
-KZ = -20 / H
-g = (KX**2 + KZ**2 + 1 / (4 * H**2)) / KX**2 * (2 * np.pi)**2 * H # omega = 2pi
-OMEGA = get_omega(g, H, KX, KZ)
 
 num_timesteps = 2e3
 NUM_SNAPSHOTS = 200
@@ -20,15 +16,12 @@ PARAMS_RAW = {'XMAX': XMAX,
               'ZMAX': ZMAX,
               'N_X': 256,
               'N_Z': 1024,
-              'OMEGA': OMEGA,
-              'KX': KX,
-              'KZ': KZ,
+              'KX': 2 * np.pi / XMAX,
+              'KZ': -20 / H,
               'H': H,
               'RHO0': 1,
-              'g': g,
               'A': 0.005,
               'F': 0.002,
-              'S': abs(1 / KZ),
               'Z0': 0.15 * ZMAX,
               'SPONGE_STRENGTH': 2,
               'SPONGE_HIGH': 0.9 * ZMAX,
@@ -37,11 +30,18 @@ PARAMS_RAW = {'XMAX': XMAX,
 
 def build_interp_params(interp_x, interp_z, overrides=None):
     params = {**PARAMS_RAW, **(overrides or {})}
+    KX = params['KX']
+    KZ = params['KZ']
+    g = (KX**2 + KZ**2 + 1 / (4 * H**2)) / KX**2 * (2 * np.pi)**2 * H # omega = 2pi
 
-    _, VG_Z = get_vg(g, H, params['KX'], params['KZ'])
+    OMEGA = get_omega(g, H, KX, KZ)
+    _, VG_Z = get_vg(g, H, KX, KZ)
     T_F = abs(ZMAX / VG_Z) * 1.2
 
     params['T_F'] = T_F
+    params['g'] = g
+    params['OMEGA'] = OMEGA
+    params['S'] = abs(1 / KZ)
     params['INTERP_X'] = interp_x
     params['INTERP_Z'] = interp_z
     params['N_X'] //= interp_x
@@ -51,29 +51,25 @@ def build_interp_params(interp_x, interp_z, overrides=None):
     return params
 
 def run(ic, name, params_dict):
-    assert 'INTERP_X' in params_dict and 'INTERP_Z' in params_dict,\
-        'params need INTERP'
     run_strat_sim(ic, name, params_dict)
-    return '%s completed' % name
 
 if __name__ == '__main__':
     tasks = [
+        (zero_ic, 'nonlinear_1',
+         build_interp_params(1, 1)),
+        (zero_ic, 'nonlinear_2',
+         build_interp_params(1, 1, overrides={'KX': 4 * np.pi / H})),
+        (zero_ic, 'nonlinear_3',
+         build_interp_params(1, 1, overrides={'KX': 16 * np.pi / H})),
+
         (zero_ic, 'linear_1',
          build_interp_params(1, 1, overrides={'F': 0.00001})),
         (zero_ic, 'linear_2',
-         build_interp_params(1, 1, overrides={'KX': 2 * np.pi / H, 'F':
-                                              0.00001})),
+         build_interp_params(1, 1, overrides={'KX': 2 * np.pi / H,
+                                              'F': 0.00001})),
         (zero_ic, 'linear_3',
-         build_interp_params(1, 1, overrides={'KX': 16 * np.pi / H, 'F':
-                                              0.00001})),
-        (zero_ic, 'nonlinear_1',
-         build_interp_params(1, 1, overrides={'F': 0.000001})),
-        (zero_ic, 'nonlinear_2',
-         build_interp_params(1, 1, overrides={'KX': 2 * np.pi / H, 'F':
-                                              0.00001})),
-        (zero_ic, 'nonlinear_3',
-         build_interp_params(1, 1, overrides={'KX': 16 * np.pi / H, 'F':
-                                              0.00001})),
+         build_interp_params(1, 1, overrides={'KX': 16 * np.pi / H,
+                                              'F': 0.00001})),
     ]
     if '-plot' not in sys.argv:
         for task in tasks:
